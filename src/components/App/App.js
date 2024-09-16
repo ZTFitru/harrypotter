9fc8dc87d9e47164b74c0fe4cb6b62f3e6ac0c1d
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-09-16 13:17:07 -0400

    Created a search bar.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,14 +5,24 @@ import { Route, Routes } from 'react-router-dom';
 import Titlepage from '../Titlepage/Titlepage';
 import SortedHouse from '../SortedHouse/SortedHouse';
 import Card from '../Card/Card';
+import { getCharacters } from '../ApiCalls';
+import { useState, useEffect } from 'react';
 
 const App = ()=> {
+
+    const [apiData, setApiData] = useState([])
+
+    useEffect(()=> {
+        getCharacters()
+        .then(data => setApiData(data))
+        .catch(err => console.log(err))
+    }, [])
 
     return (
         <main className='out-cont'>
             <Navbar />
             <Routes>
-                <Route path='/' element={<Titlepage />} />
+                <Route path='/' element={<Titlepage apiData={apiData}/>} />
                 <Route path='/:house' element={<SortedHouse />} />
                 <Route path='/character/:id' element={<Card />} />
             </Routes>
--- src/components/Titlepage/Titlepage.js
+++ src/components/Titlepage/Titlepage.js
@@ -1,35 +1,62 @@
+// import { useEffect, useState } from 'react';
+// import { getCharacters } from '../ApiCalls';
 import { useEffect, useState } from 'react';
-import { getCharacters } from '../ApiCalls';
 import defaultImage from '../../assets/default.jpg'
 import './Titlepage.css';
 import { Link } from 'react-router-dom';
+import searchLogo from '../../assets/search.png'
 
 
-const Titlepage = ()=> {
+const Titlepage = ({apiData})=> {
 
-    const [apiData, setApiData] = useState([])
+    const [userInput, setUserInput] = useState('')
+    const [filteredChar, setFilteredChar] = useState([])
+    const [isSearchVisiable, setIsSearchVisiable] = useState(false)
+
+    const searchIconClicked = ()=> {
+        setIsSearchVisiable(!isSearchVisiable)
+    }
+
 
     useEffect(()=> {
-        getCharacters()
-        .then(data => setApiData(data))
-        .catch(err => console.log(err))
-    }, [])
+        if(apiData) {
+            const result = apiData.filter(char => 
+                char.name.toLowerCase().includes(userInput.toLowerCase().trim())
+            )
+            setFilteredChar(result)
+        }
+    }, [userInput, apiData])
 
     return (
         <section className='out-cont'>
-            <div className='char-list'>
-                {apiData.map((char)=> (
-                    <Link to={`/character/${char.id}`} className='char' key={char.id}>
-                        <img src={char.image || defaultImage} alt={`Image of ${char.name}`} />
-                        <p>{char.name}</p>
-                    </Link>
-                ))}
-                {/* {apiData.map((char, index)=> {
-                    return <div key={index} className='char'>
-                        <img src={char.image || defaultImage} alt={`${char.name}`} />
-                        <p>{char.name}</p>
+            <div className='search-container'>
+                <img src={searchLogo} alt='search icon' className='search-icon' onClick={searchIconClicked} />
+                {isSearchVisiable && (
+                    <div className='search-box'>
+                        <label htmlFor='input-search'>
+                            <input 
+                                type='text'
+                                className='input-search'
+                                placeholder='Search Character...'
+                                value={userInput}
+                                onChange={(e)=> setUserInput(e.target.value)}
+                            />
+                        </label>
                     </div>
-                })} */}
+                )}
+                
+            </div>
+            <div className='char-list'>
+                {filteredChar.length > 0 ? (
+                    filteredChar.map((char)=> (
+                        <Link to={`/character/${char.id}`} className='char' key={char.id}>
+                            <img src={char.image || defaultImage} alt= {char.name} />
+                            <p>{char.name}</p>
+                        </Link>
+                    ))
+                ) : (
+                    <p className='no-char-message'>Sorry, can't find that character.</p>
+                )}
             </div>
         </section>
     )
