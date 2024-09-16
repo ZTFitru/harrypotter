5beb90b84818852465f22d56cf6eebc8b7266194
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-09-16 17:47:43 -0400

    Styled the css for the loyalty group to match the main page.

--- src/components/Loyalty/Loyalty.js
+++ src/components/Loyalty/Loyalty.js
@@ -2,6 +2,14 @@ import { useParams } from 'react-router-dom';
 import './Loyalty.css'
 import { useEffect, useState } from 'react';
 // import {sortedByGroup} from '../ApiCalls'
+import defaultImage from '../../assets/default.jpg'
+
+const sortedColors = {
+    "Dumbledore's Army": '#003366',
+    "Death Eaters": '#2F4F4F',
+    "Order of the Phoenix": '#800000',
+    "Hogwarts School of Witchcraft and Wizardry": '#F0EAD6'
+}
 
 const Loyalty = () => {
     const { loyaltyGroup } = useParams()
@@ -19,12 +27,13 @@ const Loyalty = () => {
     }, [loyaltyGroup])
 
     return (
-        <div>
+        <div className='sort-outter' style={{backgroundColor: sortedColors[loyaltyGroup]}}>
+            <h1>Characters in {loyaltyGroup}</h1>
             <div className='sort-list'>
                 {apiCharacters.map((char, index)=> {
-                    return <div key={index}>
-                        <img src={char.image} alt='' />
-                        <p>{char.name}</p>
+                    return <div className='sort-char' key={index}>
+                        <img src={char.image || defaultImage} alt='' />
+                        <h2>{char.name}</h2>
                     </div>
                 })}
             </div>
