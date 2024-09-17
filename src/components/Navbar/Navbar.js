abb6b74022b777b0bea03f13d6b1315d4364f8e1
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-09-16 22:18:14 -0400

    Updated navbar for responsive screen.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,17 @@
 import './Navbar.css'
 import menuIcon from '../../assets/menu-logo.png'
+import closeIcon from '../../assets/close-btn.png'
 import { Link, NavLink } from 'react-router-dom';
 import mainLogo from '../../assets/home-logo.jpg'
+import { useState } from 'react';
 
 const Navbar = ()=> {
+    // const [isOpen, setIsOpen] = useState(false)
+
+    // const menuImageSelected = ()=> {
+    //     console.log('hi')
+    //     setIsOpen(open => !open)
+    // }
 
 
 
@@ -13,14 +21,16 @@ const Navbar = ()=> {
                 <Link to={'/'} >
                     <img src={mainLogo} alt='Harry Potter Logo' />
                 </Link>
+                {/* <img className='nav-icon' src={isOpen ? menuIcon : closeIcon} alt='' onClick={menuImageSelected} /> */}
                 <div className='nav-right'>
-                <ul role='tablist' id='icon-links'>
+                <ul className={`icon-links `}>
                     <NavLink to={'/gryffindor'} className={'nav-g'}>Gryffindor</NavLink>
                     <NavLink to={'/hufflepuff'} className={'nav-h'}>Hufflepuff</NavLink>
                     <NavLink to={'/ravenclaw'} className={'nav-r'}>Ravenclaw</NavLink>
                     <NavLink to={'/slytherin'} className={'nav-s'}>Slytherin</NavLink>
                 </ul>
-                <img className='nav-icon' src={menuIcon} alt='' />
+                {/* <img className='nav-icon' src={isOpen ? menuIcon : closeIcon} alt='' onClick={menuImageSelected} /> */}
+                {/* <img className='nav-icon' src={closeIcon} alt='' /> */}
                 </div>
             </nav>
         </header>
