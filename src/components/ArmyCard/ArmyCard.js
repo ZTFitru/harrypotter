5d0cee562481762fdc8688364c75ba5e30e46a74
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-09-18 11:52:43 -0400

    Added styling for smaller screen for user army page.

--- src/components/ArmyCard/ArmyCard.js
+++ src/components/ArmyCard/ArmyCard.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import './ArmyCard.css'
+import { Link } from 'react-router-dom';
+import defaultImage from '../../assets/default.jpg'
 
 const ArmyCard = ()=> {
 
@@ -26,18 +28,43 @@ const ArmyCard = ()=> {
 
     return (
         <div className='army-outter'>
-            <h1>Somthing something</h1>
-            <div className='army-cont'>
-                {list.map((char, index)=> {
-                    return <div className='army-list' key={index}>
-                        <img src={char.image} alt='' />
-                        <p>{char.name}</p>
-                        <button onClick={()=> deleteCharacterBtn(char.id)}>Remove</button>
-                    </div>
-                })}
-            </div>
+            <h1>Your Army</h1>
+            {list.length > 0 ? (
+                <ul className='army-cont'>
+                    {list.map((char)=> (
+                        <li key={char.id} className='army-list'>
+                            <Link to={`/character/${char.id}`} className='army-list'>
+                                <img src={char.image || defaultImage} alt={`Headshot of ${char.name}`} />
+                                <p>{char.name}</p>
+                        {/* <button onClick={()=> deleteCharacterBtn(char.id)}>Remove</button> */}
+                            </Link>
+                            <button onClick={()=> deleteCharacterBtn(char.id)}>Remove</button>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <h2 className='empty-message'>No characters in your list</h2>
+            )}
             {/* <button onClick={()=> deleteCharacterBtn(char.id)}>Remove</button> */}
         </div>
+        // <div className='army-outter' >
+        //     <h1>Your Army</h1>
+        //     <div className='army-cont'>
+        //         {list.length > 0 ? (
+        //             list.map(char => (
+        //                 <Link to={`/character/${char.id}`} key={char.id} className='army-list' >
+        //                     <img src={char.image || defaultImage} alt={`Headshot of ${char.name}`} />
+        //                     <p>{char.name}</p>
+        //                     <div>
+        //                         <button onClick={()=> deleteCharacterBtn(char.id)}>Remove</button>
+        //                     </div>
+        //                 </Link>
+        //             ))
+        //         ) : (
+        //             <p className='empty-message'>No characters in your list</p>
+        //         )}
+        //     </div>
+        // </div>
     )
 }
 
