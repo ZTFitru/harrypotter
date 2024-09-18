3d708e8c8c066599573b8ae6a219f342ea30d06b
Zelalem Fitru <zelalemtfitru@gmail.com>
2024-09-18 15:02:01 -0400

    Updated the test for my character details page.

--- cypress/e2e/char_deatil_spec.cy.js
+++ cypress/e2e/char_deatil_spec.cy.js
@@ -1,5 +1,22 @@
 describe('template spec', () => {
+  beforeEach(()=> {
+    cy.intercept('GET', `https://harry-potter-api-gray.vercel.app/api/v1/characters`, {
+      statusCode: 200,
+      fixture: 'characters'
+    }).as('getSponge')
+
+    cy.intercept('GET', 'https://harry-potter-api-gray.vercel.app/api/v1/characters/ca3827f0-375a-4891-aaa5-f5e8a5bad225', {
+      statusCode: 200,
+      fixture: 'singleCharTest'
+    }).as('squid')
+
+    cy.visit('http://localhost:3000/')
+  })
+
+
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.get('[href="#/character/ca3827f0-375a-4891-aaa5-f5e8a5bad225"] > img').click()
+    cy.get('h1').contains('h1', 'Squidward')
+    cy.get('.char-details > :nth-child(1)').should('be.visible')
   })
 })
